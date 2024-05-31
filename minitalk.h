ef002ccbdadafcc7816019e7d2c1810f6c82a75f
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-05-31 11:33:05 +0200

    updated .h file

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 19:06:34 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/05/31 10:27:11 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/05/31 11:32:31 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ void	ft_putnbr(int n);
 void	ft_bzero(void *ptr, size_t size);
 int		ft_strlen(char *str);
 int		ft_atoi(char *str);
+void	kill_function(char *pid, char *str);
+void	signal_handler(int signal);
 
 #endif
